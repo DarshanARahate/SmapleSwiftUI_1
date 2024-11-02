d9f0bf65ddd3cfbca7769266b8c8c7e1840c67ad
darshanrahate <rahate.darshan@gmail.com>
2024-11-03 02:27:36 +0530

    Navigation Bar

--- SmapleSwiftUI_1/ContentView.swift
+++ SmapleSwiftUI_1/ContentView.swift
@@ -4,30 +4,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Group {
-                Text("Hello World..")
-                Text("Hello World..")
+        NavigationView {
+            Form {
+                Section {
+                    Text("Hello World")
+                }
             }
-            
-            Group {
-                Text("Hello World..")
-                Text("Hello World..")
-                Text("Hello World..")
-                Text("Hello World..")
-                Text("Hello World..")
-            }
-            
-            Section {
-                Text("Hello World..")
-                Text("Hello World..")
-            }
-            
-            Section {
-                Text("Hello World..")
-                Text("Hello World..")
-                Text("Hello World..")
-            }
+            .navigationTitle("SwiftUI")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
