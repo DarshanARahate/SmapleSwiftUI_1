c5d47bb52a8af3573db1dd6fb589c69d7197d4bc
darshanrahate <rahate.darshan@gmail.com>
2024-11-04 00:50:27 +0530

    Final Code Pushing

--- SmapleSwiftUI_1/ContentView.swift
+++ SmapleSwiftUI_1/ContentView.swift
@@ -3,31 +3,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Form {
-            Group {
-                Text("Hello World..")
-                Text("Hello World..")
-            }
-            
-            Group {
-                Text("Hello World..")
-                Text("Hello World..")
-                Text("Hello World..")
-                Text("Hello World..")
-                Text("Hello World..")
-            }
-            
-            Section {
-                Text("Hello World..")
-                Text("Hello World..")
-            }
-            
-            Section {
-                Text("Hello World..")
-                Text("Hello World..")
-                Text("Hello World..")
-            }
+        NavigationView {
+            Form {
+                Section {
+                    TextField(
+                        "Amount", value: $checkAmount,
+                        format: .currency(code : Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {result in
+                            Text(result, format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
+                } header : {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code:
+                                                            Locale.current.currency?.identifier ?? "USD"))
+                     
+                }
+            }.navigationTitle("Currency Page")
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        
+                        Button("Done") {
+                            amountIsFocused = false
+                        }
+                    }
+                }
         }
     }
 }
