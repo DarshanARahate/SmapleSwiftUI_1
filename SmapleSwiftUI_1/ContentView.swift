d15d27be20197f75bbe75b0ff273664ccc312137
darshanrahate <rahate.darshan@gmail.com>
2024-11-03 15:54:58 +0530

    Reading text from the user with TextField

--- SmapleSwiftUI_1/ContentView.swift
+++ SmapleSwiftUI_1/ContentView.swift
@@ -3,30 +3,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tripPercentage = 20
+    
+    let tipPercentages = [10,15,20,25,0]
+    
     var body: some View {
         Form {
-            Group {
-                Text("Hello World..")
-                Text("Hello World..")
-            }
-            
-            Group {
-                Text("Hello World..")
-                Text("Hello World..")
-                Text("Hello World..")
-                Text("Hello World..")
-                Text("Hello World..")
+
+            Section {
+                TextField("Amount", value: $checkAmount,
+                          format: .currency(
+                          code: Locale.current.currencyCode ?? "USD"))
+                .keyboardType(.decimalPad)
             }
             
             Section {
-                Text("Hello World..")
-                Text("Hello World..")
-            }
-            
-            Section {
-                Text("Hello World..")
-                Text("Hello World..")
-                Text("Hello World..")
+                Text(
+                    checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD")
+                )
             }
         }
     }
